147ff28a628e1060981df1c0e950d80ca06355c0
hellseyfer <hellseyfer@gmail.com>
2021-12-19 15:14:39 -0300

    v1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StoreModule } from '@ngrx/store';
+import { HomeComponent } from './home/containers/home/home.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    RouterModule.forRoot([
+      { path: '', pathMatch: 'full', redirectTo: 'home' },
+      { path: 'home', component: HomeComponent },
+    ]),
+    StoreModule.forRoot({}, {}),
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
